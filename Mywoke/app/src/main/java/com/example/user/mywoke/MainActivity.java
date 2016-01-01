52a1309801b1aceb1af1d4a9e8ee69d346b41759
luo1989729 <707150922@qq.com>
2016-01-01 13:35:42 +0800

    test

--- Mywoke/app/src/main/java/com/example/user/mywoke/MainActivity.java
+++ Mywoke/app/src/main/java/com/example/user/mywoke/MainActivity.java
@@ -1,37 +1,32 @@
+
+/*
+ * Copyright (c) 2015.
+ * 用于配置信息
+ *  信息公司  版权所有
+ *  OLD MAN CO.，LTD.ALL RIGHTS RESERVED.
+ */
+
 package com.example.user.mywoke;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(true){
+                int i;
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+
+
+
 }
--- Mywoke/app/src/main/java/com/example/user/mywoke/testming.java
+++ Mywoke/app/src/main/java/com/example/user/mywoke/testming.java
@@ -3,7 +3,7 @@ package com.example.user.mywoke;
 /**
  * Created by user on 2015/11/7.
  */
-public class test {
+public class testming {
 
 
 }
